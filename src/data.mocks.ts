250872aac245553eb90f0e28d7e0f3446e506b4c
unknown <dreicleal@hotmail.com>
2023-04-11 14:53:58 -0300

    cars data mock

---
+++ src/data.mocks.ts
@@ -0,0 +1,122 @@
+export const carsInfo = [
+  {
+    brand: "Ferrari",
+    model: "Enzo",
+    description:
+      "lorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsum",
+    km: "50000",
+    year: 2015,
+    price: 250000.0,
+    fuel: "gas",
+    color: "red",
+    FIPE_price: 5280000.0,
+    is_good_sale: true,
+    is_active: true,
+    images:["https://live.staticflickr.com/4811/32201118478_ccdf46f47a_o.jpg", "https://www.motortrend.com/uploads/sites/5/2018/06/2018-Ferrari-812-Superfast-front-three-quarter-in-motion.jpg?fit=around%7C875:492", "https://res.cloudinary.com/driveau/image/upload/v1644994566/cms/uploads/smzrl0hb6uwjgdweug7e.jpg"]
+  },
+  {
+    brand: "Lamborghini",
+    model: "Aventador",
+    description:
+      "lorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsum",
+    km: "50000",
+    year: 2015,
+    price: 250000.0,
+    fuel: "gas",
+    color: "black",
+    FIPE_price: 7280000.0,
+    is_good_sale: true,
+    is_active: true,
+    images:["https://s2.glbimg.com/PGGBZWKN13_HQcDwph7dchYocxg=/0x0:1080x757/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2022/N/M/CsIXsuTsKOAbijzGcMnQ/293751382-1157049234875890-2415616011868819596-n.jpg", "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjXdUWaPlHKnKgVQhl1tYWfsR1_6SWf-QPt7YXarKg-bjqAf9lzu157DE-NPiOcKLu1vGUFmgx_Kknh2sTRrAhfOlvEgqC2JYu0AscyUhXFeLQ_HrU2LhyTk4OEXftRf7XU4r51YXBPmV9v5E9ak0WkKg0aJr4gr1uUzr1f66ZUH_hNdfmo0F2DZ5b9/s1600/Aventador%20Ultimae%201.jpg", "https://www.hojeemdia.com.br/image/policy:1.860152.1638685478:1638685478/image.jpg?w=1280&"]
+  },
+  {
+    brand: "Porsche",
+    model: "Cayenne",
+    description:
+      "lorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsum",
+    km: "50000",
+    year: 2015,
+    price: 250000.0,
+    fuel: "gas",
+    color: "red",
+    FIPE_price: 280000.0,
+    is_good_sale: true,
+    is_active: true,
+    images:["https://www.notateslaapp.com/images/news/2023/model-s-plaid.jpg", "https://www.motor24.pt/files/2023/02/1676042689923_.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2FonjnRhNUpI-PZ6PvA5GM5NVhA5OLa6bJktMWhq75x6COP0R-HT6QWcQ8a3L33p4s-8&usqp=CAU"]
+  },
+  {
+    brand: "Tesla",
+    model: "Model S",
+    description:
+      "lorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsum",
+    km: "50000",
+    year: 2015,
+    price: 250000.0,
+    fuel: "gas",
+    color: "red",
+    FIPE_price: 280000.0,
+    is_good_sale: true,
+    is_active: true,
+    images:[]
+  },
+  {
+    brand: "BMW",
+    model: "X6",
+    description:
+      "lorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsum",
+    km: "50000",
+    year: 2015,
+    price: 250000.0,
+    fuel: "gas",
+    color: "red",
+    FIPE_price: 280000.0,
+    is_good_sale: true,
+    is_active: true,
+    images:["https://cdn.motor1.com/images/mgl/q4EMJ/s1/bmw-x3-2021.webp", "https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/BMW-X3-140220221442.jpg", "https://revistacarro.com.br/wp-content/uploads/2022/05/BMW-X3-M40i-M-Sport-Edition.jpg"]
+  },
+  {
+    brand: "Mercedes-Benz",
+    model: "EQS",
+    description:
+      "lorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsum",
+    km: "50000",
+    year: 2015,
+    price: 250000.0,
+    fuel: "gas",
+    color: "red",
+    FIPE_price: 280000.0,
+    is_good_sale: true,
+    is_active: true,
+    images:["https://www.motor24.pt/files/2021/10/Mercedes-EQS-450_2021-5.jpg", "https://cdn.motor1.com/images/mgl/1m4XW/s3/2022-mercedes-benz-eqs-450-exterior-front-quarter.jpg", "https://www.pappas.at/clients/pappas/global/car-model/pkw/Mercedes-EQ/EQS/basis-set/image-thumb__647270__header-slider/basis-set-Templateat_teaser-square~-~767w.png"]
+  },
+  {
+    brand: "Audi",
+    model: "A5",
+    description:
+      "lorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsum",
+    km: "50000",
+    year: 2015,
+    price: 250000.0,
+    fuel: "gas",
+    color: "red",
+    FIPE_price: 280000.0,
+    is_good_sale: true,
+    is_active: true,
+    images:["https://upload.wikimedia.org/wikipedia/commons/2/23/Audi_A5_Sportback_F5_IMG_2835.jpg", "https://dsae.s3.amazonaws.com/18036170000515/Fotos/FNU-2J62_02.jpg?u=20230228100759", "https://soulauto.com/wp-content/uploads/2022/09/00_00_00_9a733ed0-96d7-48df-8b01-28f16ab4ab6e.jpg"]
+  },
+  {
+    brand: "Hyundai",
+    model: "Ioniq 5",
+    description:
+      "lorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsumlorem Ipsum Lorem Ipsum",
+    km: "50000",
+    year: 2015,
+    price: 250000.0,
+    fuel: "gas",
+    color: "red",
+    FIPE_price: 280000.0,
+    is_good_sale: true,
+    is_active: true,
+    images:["https://www.thedrive.com/content/2021/12/2022-Hyundai-Ioniq-KL_3.jpg?quality=85","https://www.leaseplan.com/-/media/leaseplan-digital/pt/business-lease-and-private-lease/spotlight-pages/169_hyundai-ioniq-5/hyundai-ioniq_5-2022-1600-1ea-min.jpg?iar=0&rev=-1&mw=740", "https://avatars.mds.yandex.net/get-verba/1540742/2a00000180005f4d176ba055371c171c72e5/cattouch"]
+  },
+];
