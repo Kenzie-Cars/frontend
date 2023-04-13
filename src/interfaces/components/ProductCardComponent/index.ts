2c69bc5b2c5008b28a55b0944f0aa1c1c082a670
Trancoso <trancosofrancisco@gmail.com>
2023-04-13 16:02:02 -0300

    feat: product card component

--- src/interfaces/components/ProductCardComponent/index.ts
+++ src/interfaces/components/ProductCardComponent/index.ts
@@ -3,11 +3,14 @@ import {imagesResponse} from '../../images/index'
 import {IUserResponse} from '../../user/index'
 
 export interface IProductCard {
-    images: imagesResponse[],
-    model: string,
-    description: string,
-    user: IUserResponse,
-    year: number,
-    km: number,
+    id: string
+    images: imagesResponse[]
+    model: string
+    description: string
+    user: IUserResponse
+    year: number
+    km: number
     price: number
+    is_good_sale: boolean
+    is_active: boolean
 }
