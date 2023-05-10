7a255f92a577abf2a75d567cc7054575d1dc69ef
raphaeldamasceno <rdamasceno1852@gmail.com>
2023-05-10 18:25:57 -0300

    feat: add currentYear in foolter component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,19 @@
 import { AiOutlineUp } from "react-icons/ai";
 import { animateScroll as scroll } from "react-scroll";
-import { FooterStyle } from "./style";
 import logoFooter from "../../assets/Logo.png";
+import { FooterStyle } from "./style";
 
 export const Footer = () => {
   const scrollToTop = () => {
     scroll.scrollToTop();
   };
 
+  const currentYear = new Date().getFullYear();
+
   return (
     <FooterStyle>
       <img src={logoFooter} alt="Logo Motors shop" />
-      <p>© 2022 - Todos os direitos reservados.</p>
+      <p>© {currentYear} - Todos os direitos reservados.</p>
       <button onClick={scrollToTop} className="btnTop">
         <AiOutlineUp />
       </button>
