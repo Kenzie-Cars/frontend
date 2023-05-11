bf329b072048c92239c17295d6887371bdb9abee
Elton Luiz Scholze <elscholze@gmail.com>
2023-05-11 12:06:31 -0300

    fix: fixed baseURL

--- src/Requests/RequestApiKenzieKars.tsx
+++ src/Requests/RequestApiKenzieKars.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const RequestApiKenzieKars = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://kenzie-kars-backend.onrender.com",
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
